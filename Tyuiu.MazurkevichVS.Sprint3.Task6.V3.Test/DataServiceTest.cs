5bbbf9a212c5b485d0001ab4dc31f07a8ea97776
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-10-31 21:07:41 +0500

    Task6

---
+++ Tyuiu.MazurkevichVS.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MazurkevichVS.Sprint3.Task6.V3.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void SumDivisorsExpression()
+        {
+            DataService ds = new DataService();
+            int startValue = 13;
+            int stopValue = 19;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int expected = 121;
+            Assert.AreEqual(121, res);
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint3.Task6.V3/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MazurkevichVS.Sprint3.Task6.V3.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = 13;
+int stopValue = 19;
+Console.WriteLine("Начало отрезка: " + startValue);
+Console.WriteLine("Конец отрезка: " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма делителей: " + ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
